eadc04dc1eefd9b60a2b12c34d5f2953b586aedf
Ipannn <ivakaopsionalacount@gmail.com>
2022-10-18 12:30:59 +0700

    Latiahan 1 dan 2

--- src/Latihan1.java
+++ src/Latihan1.java
@@ -1,5 +1,6 @@
 public class Latihan1 {
     public static void main(String[] args) {
-        
+        System.out.println("Hello");
+        System.out.println("Selamat Belajar di program Java");
     }
 }
