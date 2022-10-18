5e43b150c9dce6fa1aa6feb6059c5bea7bb94111
Muhammad Ilham Jamaluddin <101534184+Ilhamjamal785@users.noreply.github.com>
2022-10-18 12:38:30 +0700

    Add files via upload

---
+++ src/latihan4.java
@@ -0,0 +1,14 @@
+public class latihan4 {
+    public static void main(String[] args) {
+        latihan4 motor = new latihan4();
+        motor.setMerk("");
+        motor.setNama("");
+    }
+    private void setMerk(String merk) {
+        merk = "honda";
+        System.out.println("Merk motor adalah :"+merk);
+    }
+    private void setNama(String nama) { nama = "beat";
+        System.out.println("Nama motor adalah :"+nama);
+    }
+}
